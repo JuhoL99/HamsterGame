251b94c402558964d273ad87210de4daffb11ed0
Lizene <olpertti@gmail.com>
2025-01-25 17:22:51 +0200

    hamsterspin

--- Assets/Scripts/HamsterVisuals.cs
+++ Assets/Scripts/HamsterVisuals.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class HamsterVisuals : MonoBehaviour
 {
@@ -9,30 +10,47 @@ public class HamsterVisuals : MonoBehaviour
     Transform hamster;
     float angle;
     Rigidbody ballRB;
-    public bool spinning;
-    Vector3 horiVelocityDir;
+    bool spinning;
+    Vector3 moveDir, moveAxis;
+    Quaternion storedRotation;
+    Quaternion targetRotation;
     void Start()
     {
         hamster = hamsterPivot.GetChild(0);
         ballRB = GetComponent<Rigidbody>();
-        horiVelocityDir = Vector3.forward;
+        moveDir = Vector3.forward;
+        moveAxis = Vector3.right;
     }
-
+    
     void Update()
     {
+        
+        if (spinning) return;
         float angleSpeed = ballRB.angularVelocity.magnitude;
-        Vector3 axis = ballRB.angularVelocity.normalized;
-        Vector3 horiVelocity = ballRB.velocity;
-        horiVelocity.y = 0;
-        if (horiVelocity.magnitude > 0.01f)
+        Vector3 horiAngleSpeed = ballRB.angularVelocity.normalized;
+        horiAngleSpeed.y = 0;
+        if (horiAngleSpeed.magnitude > 0.01f)
         {
-            horiVelocityDir = horiVelocity;
-            horiVelocityDir.Normalize();
+            moveAxis = horiAngleSpeed;
+            moveAxis.Normalize();
+            moveDir = Vector3.Cross(Vector3.up, moveAxis);
         }
+
         angle = Mathf.Lerp(0, angleMax, Mathf.InverseLerp(0, speedReachMaxAngle, angleSpeed));
         hamsterPivot.position = transform.position;
-        hamsterPivot.localRotation = Quaternion.AngleAxis(-angle,axis)*
-            Quaternion.LookRotation(horiVelocityDir,Vector3.up);
-
+        targetRotation = Quaternion.AngleAxis(-angle,moveAxis)*
+            Quaternion.LookRotation(moveDir,Vector3.up);
+        storedRotation = hamsterPivot.rotation;
+        hamsterPivot.localRotation = Quaternion.Slerp(hamsterPivot.rotation, targetRotation, 0.5f);
+    }
+    public void StartSpin()
+    {
+        spinning = true;
+        hamsterPivot.SetParent(transform);
+    }
+    public void StopSpin()
+    {
+        spinning = false;
+        hamsterPivot.SetParent(null);
     }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,24 +3,64 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public CinemachineFreeLook freeLookCam;
     public Transform cameraFollow;
-    public float torque;
+    public float torque, loseControlAngleSpeed, regainControlAngleSpeed, controlLostTime;
     Rigidbody rb;
     Camera cam;
+    HamsterVisuals hamsterVisuals;
+    bool inControl = true;
+    float controlLostTimer = 0f;
+    Transform hamsterPivot;
+
     void Start()
     {
         cam = Camera.main;
         rb = GetComponent<Rigidbody>();
+        hamsterVisuals = GetComponent<HamsterVisuals>();
+        hamsterPivot = hamsterVisuals.hamsterPivot;
     }
     private void LateUpdate()
     {
         cameraFollow.position = transform.position;
     }
+    void EnableHamsterRB()
+    {
+       
+    }
+    private void Update()
+    {
+        if (controlLostTimer > 0f)
+        {
+            controlLostTimer -= Time.deltaTime;
+        }
+
+    }
     void FixedUpdate()
     {
         Vector2 inputDir = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
-
+        float angleSpeed = rb.angularVelocity.magnitude;
+        
+        if (inputDir.magnitude < 0.01f && angleSpeed > loseControlAngleSpeed && inControl)
+        {
+            print("LostControl");
+            inControl = false;
+            controlLostTimer = controlLostTime;
+            hamsterVisuals.StartSpin();
+        }
+        /*
+        print(controlLostTimer);
+        print(inputDir.magnitude);
+        print(inControl);
+        print(angleSpeed);*/
+        Vector3 rotationAngles = hamsterPivot.rotation.eulerAngles;
+        if (controlLostTimer <= 0f && inputDir.magnitude > 0.01f && !inControl && (angleSpeed < regainControlAngleSpeed
+            || Quaternion.Angle(Quaternion.Euler(rotationAngles.x, 0, rotationAngles.z), Quaternion.identity) < 45f))
+        {
+            print("RegainedControl");
+            inControl = true;
+            hamsterVisuals.StopSpin();
+        }
+        if (!inControl) return;
         Vector3 forward = cam.transform.forward;
         forward.y = 0;
         forward.Normalize();
